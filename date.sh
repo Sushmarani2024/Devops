a585728351879887a9b7f32ecc147df08de67441
Sushma <Sushmarani2024>
2025-01-26 16:22:14 +0000

    date commit

---
+++ date.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Hi Sushma"
+
+echo "Today is the script day"
+
+current_date=$(date +"%A, %d-%B-%Y %H:%M:%S")
+
+echo "The current date and time is: $current_date"
+
+echo "We are all learning Scripts"
+
