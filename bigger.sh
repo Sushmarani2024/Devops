1a169d32d62493d22a2ec2cbd6300f56b55ac5d4
Sushma <Sushmarani2024>
2025-01-26 16:46:31 +0000

    bigger commit

---
+++ bigger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [$1 -gt &2 ] && [$1 -gt $3 ];
+then
+		echo "$1 is biiger number "
+		
+elif  [$1 -gt &2 ] && [$1 -gt $3 ];
+then
+	echo " $2 is the bigger number "
+	
+else
+   echo " $3 is the bigger number "
+fi 
+
