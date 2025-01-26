b4f15e657e36282df21e9867ea2d4363e1371eae
Sushma <Sushmarani2024>
2025-01-26 15:59:35 +0000

    ScriptSush commit

---
+++ scriptsush.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+name=Sushma
+	echo "my name is $name"
+contact=8653679860
+	echo "my contact number is $contact "
+place=Guntakal
+	echo "my birth place is $place"
+pincode=515801
+	echo "my pincode is $pincode"
+
+
